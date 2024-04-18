2c56b929522b66d56a00b30c43e404a1178a6c3d
Sampathkumar27896 <sampathkumar27896@hotmail.com>
2024-04-18 23:50:56 +0530

    Added unit test cases for binary-array-to-decimal problem

--- problem-solving/binary-array-to-decimal/index.test.ts
+++ problem-solving/binary-array-to-decimal/index.test.ts
@@ -1,6 +1,46 @@
 import binaryArrayToDecimal from './index';
 
+/**
+ * Sample test cases
+ * 0, 0, 0, 1 ==> 1
+ * 0, 0, 1, 0 ==> 2
+ * 0, 1, 0, 1 ==> 5
+ * 1, 0, 0, 1, 0 ==> 18
+ * 0, 0, 0, 1, 0, 1 ==> 5
+ * 0, 1, 1, 0 ==> 6
+ * 1, 1, 1, 1 ==> 15
+ * 1, 0, 1, 1 ==> 11
+ */
+describe("Test Binary Array To Decimal", () => {
+    test("Test [0, 0, 0, 1] should give 1", () => {
+        expect(binaryArrayToDecimal([0, 0, 0, 1])).toBe(1);
+    })
 
-test("binaryArrayToDecimal  return 0", () => {
-    expect(binaryArrayToDecimal([])).toBe(0);
+    test("Test [0, 0, 1, 0] should give 2", () => {
+        expect(binaryArrayToDecimal([0, 0, 1, 0])).toBe(2);
+    })
+
+    test("Test [0, 1, 0, 1] should give 5", () => {
+        expect(binaryArrayToDecimal([0, 1, 0, 1])).toBe(5);
+    })
+
+    test("Test [1, 0, 0, 1, 0] should give 18", () => {
+        expect(binaryArrayToDecimal([1, 0, 0, 1, 0])).toBe(18);
+    })
+
+    test("Test [0, 0, 0, 1, 0, 1] should give 5", () => {
+        expect(binaryArrayToDecimal([0, 0, 0, 1, 0, 1])).toBe(5);
+    })
+
+    test("Test [0, 1, 1, 0] should give 6", () => {
+        expect(binaryArrayToDecimal([0, 1, 1, 0])).toBe(6);
+    })
+
+    test("Test [1, 1, 1, 1] should give 15", () => {
+        expect(binaryArrayToDecimal([1, 1, 1, 1])).toBe(15);
+    })
+
+    test("Test [1, 0, 1, 1] should give 11", () => {
+        expect(binaryArrayToDecimal([1, 0, 1, 1])).toBe(11);
+    })
 })
--- problem-solving/binary-array-to-decimal/index.ts
+++ problem-solving/binary-array-to-decimal/index.ts
@@ -1,5 +1,16 @@
 
-
+/**
+ * Function to convert the binary numbers present in the array to equivalent decimal number
+ * @param array number[]
+ * @returns numer
+ * Time Complexity: O(n), Space Complexity: O(1)
+ */
 export default function binaryArrayToDecimal(array: number[]): number {
-    return 0;
+    let result = 0;
+    for(let i = (array.length - 1), j = 0; i >= 0; i--, j++) {
+        if(array[i] === 1) {
+            result = result + Math.pow(2, j);
+        }
+    }
+    return result;
 }
